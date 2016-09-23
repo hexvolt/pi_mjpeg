5efdd4fd3490dfc447c4abd53f0fc5c8d31033e8
hexvolt <hexvolt@gmail.com>
2016-09-23 15:40:56 +0300

    mjpeg view and service added

---
+++ pi_mjpeg/services.py
@@ -0,0 +1,25 @@
+import io
+
+import picamera
+
+
+def http_frame_generator():
+
+    stream = io.BytesIO()
+    with picamera.PiCamera() as camera:
+        camera.resolution = (320, 240)
+
+        for each_frame in camera.capture_continuous(stream, 'jpeg', quality=50, use_video_port=True):
+
+            # image is in the stream now, so rewinding to the very beginning to read it:
+            stream.seek(0)
+            frame = stream.read()
+
+            http_frame_body = b'--frame\r\n' \
+                              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+
+            yield http_frame_body
+
+            # reset stream for next frame
+            stream.seek(0)
+            stream.truncate()
--- pi_mjpeg/views.py
+++ pi_mjpeg/views.py
@@ -1,6 +1,15 @@
+from flask import render_template, Response
+
 from pi_mjpeg import app
+from services import http_frame_generator
 
 
 @app.route('/')
 def index():
-    pass
+    return render_template('index.html')
+
+
+@app.route('/mjpeg_stream')
+def mjpeg_stream():
+
+    return Response(http_frame_generator, mimetype='multipart/x-mixed-replace; boundary=frame')
